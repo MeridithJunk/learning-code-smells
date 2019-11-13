45ffbe61b3958b68afadef09ea03f2e07693974e
meridith Junk <Meridith.junk@gmail.com>
2019-11-13 09:52:27 -0600

    init project - long methods
    init project - long methods

---
+++ Bloaters/long-methods/gildedRoseKata-longMethods/gildedRoseKata-longMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gildedRoseKata_longMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
